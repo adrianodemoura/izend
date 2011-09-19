74a961c0c4fe84120095e6f147ee376cf531fd97
Adriano C. de Moura <adrianodemoura@gmail.com>
2011-09-18 21:22:21 -0300

    Tentanto fazer o bootstrap escrever na view

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -10,5 +10,19 @@
  * @subpacakge	izend.config
  */
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+	/**
+	 * Configuração para a visão
+	 * 
+	 * @return	array	$view	Objeto View
+	 */
+	protected function _initViews()
+	{
+		/*$request = new Zend_Controller_Request_Http();
+		$modulo	= $request->getModuleName();
+		$this->bootstrap('view');
+		$view = $this->getResource('view');
+		$view->modulo = $modulo;
+		return $view;*/
+	}
 }
 
